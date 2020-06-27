2210eabdbb984b78cf1a4832b9f58445e43844e5
akhsar21 <akhsarodhe@gmail.com>
2020-06-27 08:31:32 +0800

    static/js/scripts:

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,3 +10,25 @@ function showHide() {
     toggle.classList.remove("hide");
   }
 }
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+
+previews.forEach((preview) => {
+  modal.classList.add("open");
+  original.classList.add("open");
+  // Dynamyc change text and image
+  const originalSrc = preview.getAttribute("data-original");
+  original.src = `./full/${originalSrc}`;
+  const altText = preview.alt;
+  caption.textContent = altText;
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
