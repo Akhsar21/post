7d3490dae0debc609ac81c2eeddfced3f1272268
akhsar21 <akhsarodhe@gmail.com>
2020-06-28 20:11:50 +0800

    reformat

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from core.sitemaps import PostSitemap
 from django.contrib.sitemaps.views import sitemap
 from django.conf import settings
 from django.conf.urls.static import static
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
 
